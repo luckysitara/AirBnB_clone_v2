d403dacef358c9388bc0f4576408fd95f5b57d83
luckysitara <udoessienprecious@gmail.com>
2023-12-10 11:11:15 -0800

    101-setup_web_static.pp

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,33 +1,71 @@
 #!/usr/bin/python3
-"""
-Fabric script that deletes out-of-date archives
-"""
-
 from fabric.api import *
+from os.path import exists
 from datetime import datetime
+from fabric.api import local
 
 env.hosts = ['34.205.65.74', '54.173.1.197']
-env.user = 'ubuntu'
-env.key_filename = 'school'
+
+
+def do_pack():
+    '''
+    Fabric script that generates a .tgz archive from the
+    contents of the web_static
+    '''
+    try:
+        filepath = 'versions/web_static_' + datetime.now().\
+                   strftime('%Y%m%d%H%M%S') + '.tgz'
+        local('mkdir -p versions')
+        local('tar -zcvf versions/web_static_$(date +%Y%m%d%H%M%S).tgz\
+        web_static')
+        print('web_static packed: {} -> {}'.
+              format(filepath, os.path.getsize(filepath)))
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    Deploy to your webs server
+    """
+    if exists(archive_path) is False:
+        return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases'
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}'.format(file_path, file_name[:-4]))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+                                               file_path, file_name[:-4]))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}{}/web_static/* {}{}/'.format(file_path, file_name[:-4],
+                                                file_path, file_name[:-4]))
+        run('rm -rf {}{}/web_static'.format(file_path, file_name[:-4]))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(file_path,
+                                                          file_name[:-4]))
+        return True
+    except:
+        return False
+
 
 def do_clean(number=0):
-    """
-    Deletes unnecessary archives on web servers
-    """
-    number = int(number)
-    if number < 0:
-        return
-    if number == 0 or number == 1:
-        number = 1
-    else:
-        number += 1
+    '''
+    Clean extra archive files locally and on servers
+    '''
+    files = local('ls -tr versions', capture=True).split('\n')
+    number_of_files = int(number)
+    if number_of_files == 0:
+        number_of_files = 1
+
+    local_files = files[:-number_of_files]
+    for file in local_files:
+        local('rm -rf versions/{}'.format(file))
 
     with cd('/data/web_static/releases'):
-        run('ls -1t | tail -n +{} | xargs -I {} rm -rf {}'.format(number))
-
-    with cd('/data/web_static/versions'):
-        local('ls -1t | tail -n +{} | xargs -I {} rm -rf {}'.format(number))
-
-if __name__ == "__main__":
-    do_clean()
+        files = run('ls -tr').split('\n')
+        files = [f for f in files if f]  # Filter out empty strings
+        remote_files = files[:-number_of_files]
+        for file in remote_files:
+            run('rm -rf /data/web_static/releases/{}'.format(file))
 
