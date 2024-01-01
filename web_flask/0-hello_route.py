5cd8fdc26757aefac4f2f02f28c8093a4714fcf2
luckysitara <udoessienprecious@gmail.com>
2024-01-01 03:27:17 -0800

    0-setup_web_static.sh

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,11 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', app.url_map.strict_slashes = False)
+@app.route('/', strict_slashes = False)
 def hello():
     '''
     Hello Flask route
     '''
     return 'Hello HBNB!'
-
-    app.run(host='0.0.0.0, port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=50000)
