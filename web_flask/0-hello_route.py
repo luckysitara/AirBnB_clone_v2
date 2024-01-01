39d455cf7fe94aac75aa9c78bb85e66df2081a05
luckysitara <udoessienprecious@gmail.com>
2024-01-01 01:08:04 -0800

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from flask import Flask
 app = Flask(__name__)
 
+# Define a route with strict_slashes=False
+app.route('/', strict_slashes=False)
 
-@app.route('/')
 def hello():
     '''
     Hello Flask route
     '''
     return 'Hello HBNB!'
 
-
 if __name__ == '__main__':
-    app.run()
-    app.url_map.strict_slashes = False
+    # Run the app on 0.0.0.0, port 5000
+    app.run(host='0.0.0.0', port=5000)
+
