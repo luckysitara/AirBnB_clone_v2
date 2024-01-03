8843cb728c154111cb60a01614b8b9ae7c96e6dd
luckysitara <udoessienprecious@gmail.com>
2024-01-03 06:43:41 -0800

    4-number_route.py done

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -8,16 +8,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Route to display 'Hello HBNB!'
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Display 'Hello HBNB!'"""
     return 'Hello HBNB!'
+
 
 # Route to display 'HBNB'
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Display 'HBNB'"""
     return 'HBNB'
+
 
 # Route to display 'C' followed by the value of the text variable
 @app.route('/c/<text>', strict_slashes=False)
@@ -27,6 +31,8 @@ def c_text(text):
 
 # Route to display 'Python' followed by the value of the text variable
 # Default value for text is 'is cool'
+
+
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', strict_slashes=False, defaults={'text': 'is cool'})
 def python_text(text):
@@ -34,11 +40,15 @@ def python_text(text):
     return 'Python {}'.format(text.replace('_', ' '))
 
 # Route to display 'n is a number' only if n is an integer
+
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """Display 'n is a number' only if n is an integer"""
     return '{} is a number'.format(n)
 
 # Run the application on 0.0.0.0:5000
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
