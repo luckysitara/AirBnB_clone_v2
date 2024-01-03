13a436572db380d2cb15ee21da6d2aa65e12070e
luckysitara <udoessienprecious@gmail.com>
2024-01-03 07:24:52 -0800

    6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Script to start a Flask web application with specified routes.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_text(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+@app.route('/number/<int:n>')
+def number(n):
+    return '{} is a number'.format(n)
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', n=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
